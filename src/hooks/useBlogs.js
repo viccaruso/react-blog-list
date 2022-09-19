f9eb4f8005ce916ad5ed485e3a99e985c817c1b5
Victor Caruso <vic.caruso@gmail.com>
2022-09-19 14:48:47 -0700

    set useEffect for useBlogs hook to fetch supabase data

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { getBlogs } from '../services/client';
 
 export function useBlogs() {
   const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getBlogs();
+      setBlogs(data);
+    }
+    fetchData();
+  }, []);
+
+  return blogs;
 }
